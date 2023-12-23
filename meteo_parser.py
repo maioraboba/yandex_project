9070562e4bda573dd072716141b17c0554f768cb
maioraboba <113292625+maioraboba@users.noreply.github.com>
2023-12-23 21:40:59 +1000

    Update meteo_parser.py

--- meteo_parser.py
+++ meteo_parser.py
@@ -1,17 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import sys
 
-driver = webdriver.Chrome()
 
-siteUrl = "https://meteolabs.org/погода_хабаровск/завтра/"
-driver.get(siteUrl)
+class Meteo:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.siteUrl = "https://meteolabs.org/погода_хабаровск/10_дней/"
+        self.driver.get(self.siteUrl)
+        self.weather = None
+        self.t = None
+        self.sunday = None
+        self.pars_weather()
+        self.wind = None
+        self.hum = None
 
-print("Starting...")
-while True:
-    try:
-        time.sleep(0.1)
-        weather = driver.find_element(By.CLASS_NAME, "wthSBlock__commonWth").text
-        print(weather)
-    except Exception as e:
-        print("exception")
+    def pars_weather(self):
+        print("Starting...")
+        while not self.weather:
+            try:
+                time.sleep(0.1)
+                self.weather = self.driver.find_elements(By.CLASS_NAME, "wthSBlock__commonWth")
+                self.t = self.driver.find_elements(By.CLASS_NAME, "wthSBlock__interdayItem")
+                self.sunday = self.driver.find_elements(By.CLASS_NAME, "sunTime__data")
+                self.t = self.driver.find_elements(By.CLASS_NAME, "wthSBlock__interdayItem")
+                self.wind = self.driver.find_elements(By.CLASS_NAME, "wthSBlock__paramVal")[::4]
+                self.hum = self.driver.find_elements(By.CLASS_NAME, "wthSBlock__paramVal")[2::4]
+                for i in self.hum:
+                    print(i.text)
+            except Exception as e:
+                print("exception")
+
+
+if __name__ == '__main__':
+    ex = Meteo()
+    sys.exit()
